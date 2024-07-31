bdcc1ed40c37291e9bbeec2b0461d20012e10789
ahmedob21 <ahmedob22@gmail.com>
2024-08-01 00:55:56 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    googleMapsApiKey: 'AIzaSyCVWrsGpfiyZc3brrcKXpDth8nDR3TF3nA'
+    googleMapsApiKey: 'ApiKey'
   };
